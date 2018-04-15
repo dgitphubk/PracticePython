c869ad048da6ac112328be61f1f6957a03b34419
dgitphubk <dgitphubk@users.noreply.github.com>
2018-04-15 14:50:10 -0700

    Exercise 9 completed
    Thought I'd finished it already but I was mistaken.

--- Exercise 9.py
+++ Exercise 9.py
@@ -6,27 +6,26 @@ import string
 a = 0
 b = random.randint(1, 9)
 guess = 0
+again = 'Y'
 
 print('I want to play a game.')
-# while True:
-	# guess = input('Guess a number between 1 and 9 or exit: ')
-	# if guess.lower() == 'exit':
-		# print('Goodbye.')
-		# break
-	# elif int(guess) != b:
-		# guess = int(guess)
-		# while guess != b:
-			# if guess < b:
-				# a = a + 1
-				# guess = input('Too low. Guess again or exit: ')
-				# if guess.lower() != 'exit':
-					# guess = int(guess)
-			# else:
-				# a = a + 1
-				# guess = input('Too high. Guess again or exit: ')
-				# if guess.lower() != 'exit':
-					# guess = int(guess)
-	# else:
-		# guess = int(guess)
-
-# print('You win! It took you {} guesses.').format(a)
+while again == 'Y':
+	while guess != b and guess != 'exit':
+		guess = input('Pick a number between 1 and 9, or exit: ')
+		if guess == 'exit':
+			break
+		guess = int(guess)
+		a += 1
+		if guess < b:
+			print('Too low.')
+		elif guess > b:
+			print('Too high.')
+		else:
+			print('You won the game in', a, 'attempts.')
+	again = input('Play again Y/N? ').upper()
+	if again == 'N':
+		print('Bye.')
+	else:
+		guess = 0
+		a = 0
+		b = random.randint(1, 9)
